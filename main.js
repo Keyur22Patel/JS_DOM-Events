c8cc5c2454e790c03cc8042ddc440d54923ed454
_Keyur Patel_ <_[keyur22p@gmail.com]_>
2022-06-17 13:16:45 -0500

    DOM

--- main.js
+++ main.js
@@ -1,47 +1,40 @@
 console.log("Script attached");
-/*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
-// Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
+let node1 = document.getElementById("node1");
+node1.textContent = " I used the getElementById(\"node1\") method to access this"; 
+let node2 = document.getElementByClassName("node2");
+node2.textContent = " I used the getElementByClassName(\"node2\") method to access this"; 
+let h3s = document.getElementByTagName("h3s");
+for (let h3 of h3s)
+h3.textContent = " I used the getElementByTagName(\"h3\") method to access this"; 
+ 
 
-// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+let p = document.childElement("p");
+p.textContent = "This node was creaed using thr createElement() method";
 
-// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+let parent2 = document.querySelector("#parent");
+parent2.appendChild(p);
+ 
+let link = document.createElement("a");
+link.textContent = "Google Search";
+link.href = "https://www.google.com/";
 
-/*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
+parent2.insertBefore(link, p);
 
-// TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
+let child = document.childElement("p");
+child.textContent = "New Child Node";
 
-// TODO: Append the created node to the parent node using the element.appendChild() method
+let parent3 = document.querySelector("#exercise-container3");
+let replecsChild = document.querySelector("#N1");
+parent3.replaceChild(child, replecsChild);
 
-// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
+setTimeout(() => {
+    parent3.removeChild(child);
+}, 5000);
 
-// BONUS: Add a link href to the <a>
-
-// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
-
-/*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
-
-// TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
-
-// TODO: Remove the "New Child Node"
-
-/*----------- Exercise #4: ANIMATIONS ----------- */
-// Use the following array of values to generate a list on the DOM
 
 let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
+  
+let container4 = document.querySelector("#container");
+container4.appendChild(ul);
 
-
-// TODO: Create an unordered list element
-
-// TODO: Iterate over the array values, and create a list item element for each
-
-// TODO: Append the new list items to the unordered list element
-
-// TODO: Append the unordered list to the `div#container` under exercise 4 
-
-/*----------- Exercise #5: DOM EVENTS --------------*/
-
-// TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
-// -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
-// This div should be a 'modal' that covers the main content on the screen
-// BONUS: The modal popup should be able to be closed. Refactor for this functionality
